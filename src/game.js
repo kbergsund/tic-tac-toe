ef6ad56bcb3c303308861a9f18316691197dc197
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-24 10:47:14 -0700

    Add win processing & game reset

--- src/game.js
+++ src/game.js
@@ -4,34 +4,37 @@ class Game {
     this.player2 = new Player(2, '🍊');
     this.board = ["", "", "", "", "", "", "" ,"" ,""]; // [ 0, 1, 2, 3, 4, 5, 6, 7, 8] will correspond to sections on gameboard
     this.turn = this.player1;
+    this.win = false;
   }
 
   playGame() {
+    console.log(this.board);
     // loops through max number of turns in a game
     for (var i = 0; i < 9; i++) {
       // if win is false
-      if (!this.checkForWin()) {
-        // calls method within Player. Drafted using prompt() right now while I am not touching the DOM. Player should input tile number where they want to place their token- this may move here or even to main.js eventually because I'll be using event delegation.
-        var play = this.turn.takeTurn(this);
+      if (!this.win) {
+        // // calls method within Player. Drafted using prompt() right now while I am not touching the DOM. Player should input tile number where they want to place their token- this may move here or even to main.js eventually because I'll be using event delegation.
 
-        // preliminary error handling for when a spot is already taken by a token. ONLY WORKS ONCE, needs refactoring...
-        if (!this.board[play]) {
+        // preliminary error handling for when a spot is already taken by a token. ONLY WORKS ONCE, needs refactoring... a while loop?
+        if (this.board[play] === "") {
           this.board[play] = this.turn.token;
         } else {
           // Sets element within board array- index corresponds to user input -to the player emoji.
           var play = this.turn.takeTurn(this);
+          this.board[play] = this.turn.token;
         }
+
+        // var play = this.turn.takeTurn(this);
+        // this.board[play] = this.turn.token;
 
         console.log(this.board);
         this.toggleTurn();
         this.checkForWin();
+        this.checkForDraw();
 
-        if (!this.board.includes('')) {
-          console.log('draw ' + this.checkForDraw());
-        }
-      } else {
-        console.log('win ' + this.checkForWin());
-        // return this.checkForWin();
+      } else if (this.win) {
+        this.processWin(this.winningToken);
+        return;
       }
     }
   }
@@ -42,13 +45,11 @@ class Game {
     } else {
       this.turn = this.player1;
     }
-    // Invoke at the END of a turn, once player has clicked the tile they wish to place.
     // DOM - toggle a turn class? Or just update innerText of h1 based on the property value after it runs?
   }
 
   checkForWin() {
     // can be further refactored. Use conditional logic to set num1, 2, & 3 based on boardValue. Then go into comparing them.
-    // this.win = false;
     var winningNumbers = [0, 1, 2, 3, 6];
     for (var i = 0; i < winningNumbers.length; i++) {
       var boardValue = winningNumbers[i];
@@ -60,7 +61,10 @@ class Game {
           var num3 = this.board[boardValue + 2];
           console.log(num1, num2, num3);
           if (num1 === num2 && num1 === num3) {
-            return true;
+            this.win = true;
+            // sets new property to the token that is in the winning spots. This is used in the processWin() function
+            this.winningToken = this.board[boardValue];
+            // return this.board[boardValue];
           }
         }
       }
@@ -72,7 +76,8 @@ class Game {
           var num3 = this.board[boardValue + 6];
           console.log(num1, num2, num3);
           if (num1 === num2 && num1 === num3) {
-            return true;
+            this.win = true;
+            this.winningToken = this.board[boardValue];
           }
         }
       }
@@ -85,7 +90,8 @@ class Game {
             var num3 = this.board[boardValue + 8];
             console.log(num1, num2, num3);
             if (num1 === num2 && num1 === num3) {
-              return true;
+              this.win = true;
+              this.winningToken = this.board[boardValue];
             }
           } else if (i === 2) {
             var num1 = this.board[boardValue];
@@ -93,7 +99,8 @@ class Game {
             var num3 = this.board[boardValue + 4];
             console.log(num1, num2, num3);
             if (num1 === num2 && num1 === num3) {
-              return true;
+              this.win = true;
+              this.winningToken = this.board[boardValue];
             }
           }
         }
@@ -108,20 +115,38 @@ class Game {
     // 3: horizontal
     // 6: horizontal
 
+// draw 0 2 3 4 7
+// draw 1 5 6 8
+
+  processWin(winningToken) {
+    if (Object.values(this.player1).includes(winningToken)) {
+      console.log('player1 wins!');
+      this.player1.winGame();
+    } else if (Object.values(this.player2).includes(winningToken)) {
+      console.log('player2 wins!');
+      this.player2.winGame();
+    }
+  }
+
   checkForDraw() {
-    if (!this.checkForWin()) {
+    if (!this.board.includes('')) {
+      console.log('draw ' + true);
       return true;
     } else {
       return false;
     }
-    // be invoked if checkForWin returns false and if there are no more empty spaces in the board array i.e. (length === 9)
-      // if true, DOM should update accordingly.
+      // if true, DOM should update accordingly. Check demo.
   }
 
   resetGame() {
-    // reset board & turns (main.js will handle resetting DOM)
+    this.board = ["", "", "", "", "", "", "" ,"" ,""];
+    this.turn = this.player1;
+    console.log(this.board);
+    // main.js will handle resetting DOM.
+    // research Timeout for when to call this and restart playGame()
   }
 }
 
 var game1 = new Game;
 game1.playGame();
+game1.resetGame();
--- src/player.js
+++ src/player.js
@@ -17,6 +17,16 @@ class Player {
 
   takeTurn(game) {
     var play = parseInt(prompt('Which tile?'));
+    // Trying to refactor error handling when a spot is already taken. For some reason the play variable is not updating the second time takeTurn is called. Likely because I don't fully understand recursive function calls yet? May just have to be sorted while working on DOM.
+    // if (game.board[play] === "") {
+      // console.log('prompt value ' + play);
     return play;
+    // } else {
+    //   this.takeTurn(game);
   }
+
+    winGame() {
+      this.wins += 1;
+      console.log(this.wins);
+    }
 }
