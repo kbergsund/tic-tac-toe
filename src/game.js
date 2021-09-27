952978c8032571d933ca99b14609c93d4a5f75c0
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-26 19:18:46 -0700

    Implement localStorage for player wins

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 class Game {
-  constructor() {
+  constructor(player1, player2) {
     this.player1 = new Player(1, '🦉');
     this.player2 = new Player(2, '🦝');
     this.board = ["", "", "", "", "", "", "" ,"" ,""];
@@ -10,8 +10,7 @@ class Game {
     if (this.board[tileClicked] === "") {
       this.board[tileClicked] = this.turn.token;
     } else {
-      console.log('access denied');
-      // toggleTurn() is called twice- double negative (here and in main.js) -as error handling solution.
+      // error handling
       this.toggleTurn();
     }
   }
@@ -28,48 +27,41 @@ class Game {
     var winningNumbers = [0, 1, 2, 3, 6];
 
     for (var i = 0; i < winningNumbers.length; i++) {
-      if (i === 0) {
-        var a = 1;
-        var b = 2;
-        var c = 3;
-        var d = 6;
-        var e = 4;
-        var f = 8;
-      } else if (i === 1) {
-        var c = 3;
-        var d = 6;
-      } else if (i === 2) {
-        var c = 3;
-        var d = 6;
-        var e = 2;
-        var f = 4;
-      } else if (i === 3 || i === 4) {
-        var a = 1;
-        var b = 2
-      }
-
       var num1 = winningNumbers[i];
       var boardValue = this.board[num1];
-      var num2 = this.board[num1 + a];
-      var num3 = this.board[num1 + b];
-      var num4 = this.board[num1 + c];
-      var num5 = this.board[num1 + d];
-      var num6 = this.board[num1 + e];
-      var num7 = this.board[num1 + f];
+      var num2 = this.board[num1 + 1];
+      var num3 = this.board[num1 + 2];
+      var num4 = this.board[num1 + 3];
+      var num5 = this.board[num1 + 6];
+      if (i === 0) {
+        var num6 = this.board[num1 + 4];
+        var num7 = this.board[num1 + 8];
+      } else if (i === 2) {
+        var num6 = this.board[num1 + 2];
+        var num7 = this.board[num1 + 4];
+      }
 
       if (boardValue !== "") {
         // horizontal
-        if (boardValue === num2 && boardValue === num3) {
-          this.winningToken = boardValue;
-          return true;
+        if (i === 0 || i === 3 || i === 4) {
+          if (boardValue === num2 && boardValue === num3) {
+            this.winningToken = boardValue;
+            return true;
+          }
+        }
         // vertical
-        } else if (boardValue === num4 && boardValue === num5) {
-          this.winningToken = boardValue;
-          return true;
+        if (i === 0 || i === 1 || i === 2) {
+          if (boardValue === num4 && boardValue === num5) {
+            this.winningToken = boardValue;
+            return true;
+          }
+        }
         // diagonal
-        } else if (boardValue === num6 && boardValue === num7) {
-          this.winningToken = boardValue;
-          return true;
+        if (i === 0 || i === 2) {
+          if (boardValue === num6 && boardValue === num7) {
+            this.winningToken = boardValue;
+            return true;
+          }
         }
       }
     }
@@ -77,12 +69,16 @@ class Game {
 
   processWin(winningToken) {
     if (Object.values(this.player1).includes(winningToken)) {
-      this.player1.winGame();
-      return this.player1.id;
+      var winnerID = this.player1.id;
     } else if (Object.values(this.player2).includes(winningToken)) {
-      this.player2.winGame();
-      return this.player2.id;
+      var winnerID = this.player2.id;
     }
+    
+    var interpolatedWinner = `player${winnerID}`;
+    this[interpolatedWinner].winGame();
+    this.player1.saveWinsToStorage();
+    this.player2.saveWinsToStorage();
+    return winnerID;
   }
 
   checkForDraw() {
@@ -96,5 +92,11 @@ class Game {
   resetGame() {
     this.board = ["", "", "", "", "", "", "" ,"" ,""];
     this.turn = this.player1;
+    if (localStorage.length !== 0) {
+      var player1Refresh = game.player1.retrieveWinsFromStorage();
+      var player2Refresh = game.player2.retrieveWinsFromStorage();
+      game.player1.wins = player1Refresh.wins;
+      game.player2.wins = player2Refresh.wins;
+    }
   }
 }
