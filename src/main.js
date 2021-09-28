30b430e5cf6be7a58ebd1be7a7049755b74661e7
Kyra Bergsund <k.bergsund@gmail.com>
2021-09-28 10:37:07 -0700

    Add clear button & update grid hover states

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-var game = new Game();
-
+// Query Selectors
 var gameBoard = document.querySelector('#gameBoard');
 var winCount = document.querySelectorAll('#winCount');
 
+// Event Listeners
 window.addEventListener('load', generateGrid);
 gameBoard.addEventListener('click', playGame);
+// gameBoard.addEventListener('click', clearDOM);
 
+// Global Variables
+var game = new Game();
+
+// Functions & Event Handlers
 function generateGrid() {
   gameBoard.childNodes[1].innerText = `It's ${game.turn.token}'s turn`;
   var grid = gameBoard.childNodes[3];
@@ -21,30 +26,34 @@ function generateGrid() {
   persistWins();
 }
 
-function playGame() {
-  updateGrid();
-  evaluateGame();
-}
-
-function updateGrid() {
-  var grid = gameBoard.childNodes[3];
-  var tileClicked = event.target.className;
-  game.takeTurn(tileClicked);
-  for (var i = 0; i < grid.childNodes.length; i++) {
-    var tileToUpdate = grid.childNodes[i];
-    var tileNumber = grid.childNodes[i].className;
-    if (tileNumber === tileClicked) {
-      tileToUpdate.innerHTML = `<p>${game.board[tileNumber]}<p>`
-      console.log(tileToUpdate.classList);
-    }
-  }
-}
-
 function persistWins() {
   for (var i = 0; i < 2; i++) {
     var player = i + 1;
     var winsToDisplay = game[`player${player}`].wins;
     winCount[i].childNodes[3].innerText = `${winsToDisplay} wins`;
+  }
+}
+
+function playGame() {
+  updateGrid();
+  evaluateGame();
+  if (event.target.type === 'button') {
+    clearDOM();
+  }
+}
+
+function updateGrid() {
+  var grid = gameBoard.childNodes[3];
+  var tileClicked = event.target.classList;
+  game.takeTurn(tileClicked);
+  for (var i = 0; i < grid.childNodes.length; i++) {
+    var tileToUpdate = grid.childNodes[i];
+    var tileNumber = grid.childNodes[i].classList;
+    console.log("tile number  " + tileNumber);
+    if (tileNumber === tileClicked) {
+      tileToUpdate.innerHTML = `<p>${game.board[tileNumber]}<p>`
+      tileClicked.add('disabled');
+    }
   }
 }
 
@@ -71,5 +80,12 @@ function displayWin() {
 
 function resetPlay() {
   game.resetGame();
-  setTimeout(generateGrid, 1500);
+  setTimeout(generateGrid, 1800);
+}
+
+function clearDOM() {
+  // if (event.target.type === 'button') {
+    game.clearScores();
+    generateGrid();
+  // }
 }
--- src/player.js
+++ src/player.js
@@ -10,15 +10,12 @@ class Player {
     var storedObject = localStorage.setItem(`${this.id}`, stringifiedObject);
   }
 
-  // add this into generateGrid function.
   retrieveWinsFromStorage() {
     var retrievedObject = localStorage.getItem(`${this.id}`);
     var parsedObject = JSON.parse(retrievedObject);
-    
     return parsedObject;
   }
 
-  // perhaps this is not necessary... just put it in game.js...
   winGame() {
     this.wins += 1;
   }
